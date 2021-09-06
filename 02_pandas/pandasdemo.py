77b73c5dc08db9279b6ad321e27ade2f440a72e2
Altesmi <optikkanen@gmail.com>
2021-09-06 20:54:47 +0300

    Lisätty osioon 02 notebook

--- 02_pandas/pandasdemo.py
+++ 02_pandas/pandasdemo.py
@@ -8,29 +8,34 @@ data = pd.read_csv('../data/osakkeet.csv')
 print(data.head())
 
 # Aluksi päivät ovat vain stringejä!
-data['PVM'] = pd.to_datetime(data['PVM'], infer_datetime_format=True)
+data['PVM'] = pd.to_datetime(data['PVM'], format='%d/%m/%Y')
 # # TAI data['PVM'] = pd.to_datetime(data['PVM'], format='%d/%m/%Y')
 # # TAI parse_dates=['PVM']
 print(data['PVM'])
 print(data['PVM'].dtype)
 
 # Sarakkeen valitseminen
-dataNokia = data[['PVM', 'Nokia']]
+# Lisäys videon jälkeen. Ottamalla kopion data DataFramesta vältetään varoitus
+# "A value is trying to be set on a copy of a slice from a DataFrame", joka voi
+# olla joissain tapauksissa ongelmallista.
+# Asiasta lisää: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+
+dataNokia = data[['PVM', 'Nokia']].copy()
+
 # print(dataNokia.head())
 
-# Sarakkeen lissääminen
+# Sarakkeen lisääminen
 
-dataNokia['juoksevaKA'] = dataNokia['Nokia'].rolling(window=30).mean().fillna(method='ffill')
+dataNokia['juoksevaKA'] = dataNokia['Nokia'].rolling(window=5).mean().fillna(method='ffill')
 
 print(dataNokia.head())
 
-#Sarakkeen lisääminen
+# Rivin valitseminen
+print(dataNokia[10:20:2])
+print(dataNokia.loc[dataNokia['PVM'] > datetime(2020,4,29), :])
 
-#Rivin valitseminen
-# print(dataNokia[10:20:2])
-print(dataNokia[data['PVM'] > datetime(2020,4,29)])
-
-#Flip 
+# Käännetään dataframe ympäri (viimeinen rivi ensimmäiseksi).
+# Jos 'PVM' olisi asetettu indeksiksi, reset_index(drop=True) ei tarvittaisi
 dataNokia = dataNokia[::-1].reset_index(drop=True)
 
 print(dataNokia.head())
@@ -44,9 +49,9 @@ print(maski2)
 print(maski1 & maski2)
 print(dataNokia[maski1 & maski2])
 
-# print(dataNokia[((dataNokia['PVM'] > datetime(2011,5,1)) & (dataNokia['PVM'] < datetime(2011,5,29)))])
+print(dataNokia[((dataNokia['PVM'] > datetime(2011,5,1)) & (dataNokia['PVM'] < datetime(2011,5,29)))])
 
-# Plot
+# Tehdään kuvaaja Nokian keskihinnasta ja juoksevasta keskiarvosta
 ax = dataNokia.plot(x='PVM', y=['Nokia', 'juoksevaKA'], linewidth=3, fontsize=18, title='Nokia')
 ax.set_xlabel('PVM',fontsize=18)
 ax.set_ylabel('Hinta (€)', fontsize=18)
